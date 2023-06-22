7914c008a0b8436cfd0c93b547409e477fbd81d6
Vishesh Pahwa <73529277+visheshpahwa@users.noreply.github.com>
2023-06-22 22:13:47 +0530

    user profile fragment created

--- app/src/main/java/fragment/UserProfileFragment.kt
+++ app/src/main/java/fragment/UserProfileFragment.kt
@@ -13,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.example.voluntrix_app.LoginActivity
 import com.example.voluntrix_app.R
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 
@@ -22,6 +23,13 @@ import com.google.firebase.auth.FirebaseAuth
  * create an instance of this fragment.
  */
 class UserProfileFragment : Fragment() {
+
+    lateinit var navigationView: NavigationView
+
+    lateinit var userName:TextView
+    lateinit var userImg:ImageView
+    lateinit var userEmail:TextView
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +41,26 @@ class UserProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view=inflater.inflate(R.layout.fragment_user_profile, container, false )
+
+        userImg=view.findViewById(R.id.userImg)
+        userEmail=view.findViewById(R.id.userEmail)
+        userName=view.findViewById(R.id.userName)
+
+
+        val account = GoogleSignIn.getLastSignedInAccount(this.requireContext())
+        if (account!=null){
+            val name = account.displayName
+            val email = account.email
+            val photo = account.photoUrl
+            userName.setText(name)
+            userEmail.setText(email)
+            Glide.with(this).load(photo).into(userImg);
+        }
+        auth = FirebaseAuth.getInstance()
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false)
+        return view
     }
 }
